2fa11ca73a37b9265624bd123e2fc7b99e2d926a
Casey Rodarmor <casey@rodarmor.com>
2020-01-04 20:48:36 -0600

    Enable and fix lints

---
+++ src/err.rs
@@ -0,0 +1,8 @@
+macro_rules! err {
+  ($env:expr, $fmt:expr) => {
+    write!($env.err, $fmt).context(crate::error::Stderr)?;
+  };
+  ($env:expr, $fmt:expr, $($arg:tt)*) => {
+    write!($env.err, $fmt, $($arg)*).context(crate::error::Stderr)?;
+  };
+}
