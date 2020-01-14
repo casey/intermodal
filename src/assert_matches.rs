f8c907184e77484ea780eec68606c08ab9e6505f
Casey Rodarmor <casey@rodarmor.com>
2020-01-14 00:52:27 -0800

    Redirect stdin and stdout and capture for tests (#64)

---
+++ src/assert_matches.rs
@@ -0,0 +1,12 @@
+macro_rules! assert_matches {
+  ($expression:expr, $( $pattern:pat )|+ $( if $guard:expr )?) => {
+    match $expression {
+      $( $pattern )|+ $( if $guard )? => {}
+      left => panic!(
+        "assertion failed: (left ~= right)\n  left: `{:?}`\n right: `{}`",
+        left,
+        stringify!($($pattern)|+ $(if $guard)?)
+      ),
+    }
+  }
+}
--- src/env.rs
+++ src/env.rs
@@ -4,6 +4,7 @@ pub(crate) struct Env {
   args: Vec<String>,
   dir: Box<dyn AsRef<Path>>,
   pub(crate) err: Box<dyn Write>,
+  pub(crate) _out: Box<dyn Write>,
 }
 
 impl Env {
@@ -13,16 +14,17 @@ impl Env {
       Err(error) => panic!("Failed to get current directory: {}", error),
     };
 
-    Self::new(dir, io::stderr(), env::args())
+    Self::new(dir, io::stdout(), io::stderr(), env::args())
   }
 
   pub(crate) fn run(&mut self) -> Result<(), Error> {
     Opt::from_iter_safe(&self.args)?.run(self)
   }
 
-  pub(crate) fn new<D, E, S, I>(dir: D, err: E, args: I) -> Self
+  pub(crate) fn new<D, O, E, S, I>(dir: D, out: O, err: E, args: I) -> Self
   where
     D: AsRef<Path> + 'static,
+    O: Write + 'static,
     E: Write + 'static,
     S: Into<String>,
     I: IntoIterator<Item = S>,
@@ -31,13 +33,23 @@ impl Env {
       args: args.into_iter().map(Into::into).collect(),
       dir: Box::new(dir),
       err: Box::new(err),
+      _out: Box::new(out),
     }
   }
 
   pub(crate) fn status(&mut self) -> Result<(), i32> {
+    use structopt::clap::ErrorKind;
     if let Err(error) = self.run() {
-      write!(&mut self.err, "error: {}", error).ok();
-      Err(EXIT_FAILURE)
+      if let Error::Clap { source } = error {
+        write!(&mut self.err, "{}", source).ok();
+        match source.kind {
+          ErrorKind::VersionDisplayed | ErrorKind::HelpDisplayed => Ok(()),
+          _ => Err(EXIT_FAILURE),
+        }
+      } else {
+        write!(&mut self.err, "error: {}", error).ok();
+        Err(EXIT_FAILURE)
+      }
     } else {
       Ok(())
     }
@@ -45,5 +57,27 @@ impl Env {
 
   pub(crate) fn resolve(&self, path: impl AsRef<Path>) -> PathBuf {
     self.dir.as_ref().as_ref().join(path).clean()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn error_message_on_stdout() {
+    let mut env = testing::env(
+      ["torrent", "create", "--input", "foo", "--announce", "bar"]
+        .iter()
+        .cloned(),
+    );
+    fs::write(env.resolve("foo"), "").unwrap();
+    env.status().ok();
+    let err = env.err();
+    if !err.starts_with("error: Failed to parse announce URL:") {
+      panic!("Unexpected standard error output: {}", err);
+    }
+
+    assert_eq!(env.out(), "");
   }
 }
