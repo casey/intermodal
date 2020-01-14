8035b3409f63d0e125532c23f6f7b013350f23c1
Casey Rodarmor <casey@rodarmor.com>
2020-01-14 03:51:59 -0800

    Test usecolor parsing

--- src/use_color.rs
+++ src/use_color.rs
@@ -24,3 +24,18 @@ impl FromStr for UseColor {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn from_str() {
+    assert_eq!(UseColor::Auto, AUTO.parse().unwrap());
+    assert_eq!(UseColor::Always, ALWAYS.parse().unwrap());
+    assert_eq!(UseColor::Never, NEVER.parse().unwrap());
+    assert_eq!(UseColor::Auto, AUTO.to_uppercase().parse().unwrap());
+    assert_eq!(UseColor::Always, ALWAYS.to_uppercase().parse().unwrap());
+    assert_eq!(UseColor::Never, NEVER.to_uppercase().parse().unwrap());
+  }
+}
