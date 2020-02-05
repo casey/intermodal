b94bc3749486378aa32134c6f1bda143ac5212a1
Casey Rodarmor <casey@rodarmor.com>
2020-02-05 15:54:55 -0800

    Fix for windows

--- src/platform.rs
+++ src/platform.rs
@@ -14,7 +14,10 @@ impl PlatformInterface for Platform {
   }
 
   fn hidden(path: &Path) -> Result<bool, Error> {
+    use std::os::windows::fs::MetadataExt;
+
     const HIDDEN_MASK_WIN: u32 = 0x0000_0002;
+
     let metadata = path.metadata().context(error::Filesystem { path })?;
     Ok((metadata.file_attributes() & HIDDEN_MASK_WIN) != 0)
   }
@@ -27,8 +30,10 @@ impl PlatformInterface for Platform {
   }
 
   fn hidden(path: &Path) -> Result<bool, Error> {
-    const HIDDEN_MASK_MAC: u32 = 0x0000_8000;
     use std::os::unix::ffi::OsStrExt;
+    use std::{ffi::CString, mem};
+
+    const HIDDEN_MASK_MAC: u32 = 0x0000_8000;
 
     let mut stat: libc::stat = unsafe { mem::zeroed() };
 
