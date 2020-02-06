70f8b90edfb51c6c6a48a1b4f9eac3ab038f4a31
Casey Rodarmor <casey@rodarmor.com>
2020-02-06 14:54:11 -0800

    Don't display tier labels if tiers only contain a single entry

--- src/opt/torrent/show.rs
+++ src/opt/torrent/show.rs
@@ -122,4 +122,187 @@ Files\tfoo
       assert_eq!(have, want);
     }
   }
+
+  #[test]
+  fn tier_list_with_main() {
+    let metainfo = Metainfo {
+      announce: "a".into(),
+      announce_list: Some(vec![vec!["x".into()], vec!["y".into()], vec!["z".into()]]),
+      comment: Some("comment".into()),
+      created_by: Some("created by".into()),
+      creation_date: Some(1),
+      encoding: Some("UTF-8".into()),
+      info: Info {
+        private: Some(1),
+        piece_length: 16 * 1024,
+        source: Some("source".into()),
+        name: "foo".into(),
+        pieces: vec![
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        ],
+        mode: Mode::Single {
+          length: 20,
+          md5sum: None,
+        },
+      },
+    };
+
+    {
+      let mut env = TestEnvBuilder::new()
+        .arg_slice(&["imdl", "torrent", "show", "--input", "foo.torrent"])
+        .out_is_term()
+        .build();
+
+      let path = env.resolve("foo.torrent");
+
+      metainfo.dump(path).unwrap();
+
+      env.run().unwrap();
+
+      let have = env.out();
+      let want = "        Name  foo
+     Comment  comment
+     Created  1970-01-01 00:00:01 UTC
+  Created By  created by
+      Source  source
+   Info Hash  b7595205a46491b3e8686e10b28efe7144d066cc
+Torrent Size  240 bytes
+Content Size  20 bytes
+     Private  yes
+    Trackers  a
+              x
+              y
+              z
+  Piece Size  16 KiB
+ Piece Count  1
+  File Count  1
+       Files  foo
+";
+
+      assert_eq!(have, want);
+    }
+
+    {
+      let mut env = TestEnvBuilder::new()
+        .arg_slice(&["imdl", "torrent", "show", "--input", "foo.torrent"])
+        .build();
+
+      let path = env.resolve("foo.torrent");
+
+      metainfo.dump(path).unwrap();
+
+      env.run().unwrap();
+
+      let have = env.out();
+      let want = "\
+Name\tfoo
+Comment\tcomment
+Created\t1970-01-01 00:00:01 UTC
+Created By\tcreated by
+Source\tsource
+Info Hash\tb7595205a46491b3e8686e10b28efe7144d066cc
+Torrent Size\t240
+Content Size\t20
+Private\tyes
+Trackers\ta\tx\ty\tz
+Piece Size\t16384
+Piece Count\t1
+File Count\t1
+Files\tfoo
+";
+
+      assert_eq!(have, want);
+    }
+  }
+
+  #[test]
+  fn tier_list_without_main() {
+    let metainfo = Metainfo {
+      announce: "a".into(),
+      announce_list: Some(vec![vec!["b".into()], vec!["c".into()], vec!["a".into()]]),
+      comment: Some("comment".into()),
+      created_by: Some("created by".into()),
+      creation_date: Some(1),
+      encoding: Some("UTF-8".into()),
+      info: Info {
+        private: Some(1),
+        piece_length: 16 * 1024,
+        source: Some("source".into()),
+        name: "foo".into(),
+        pieces: vec![
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        ],
+        mode: Mode::Single {
+          length: 20,
+          md5sum: None,
+        },
+      },
+    };
+
+    {
+      let mut env = TestEnvBuilder::new()
+        .arg_slice(&["imdl", "torrent", "show", "--input", "foo.torrent"])
+        .out_is_term()
+        .build();
+
+      let path = env.resolve("foo.torrent");
+
+      metainfo.dump(path).unwrap();
+
+      env.run().unwrap();
+
+      let have = env.out();
+      let want = "        Name  foo
+     Comment  comment
+     Created  1970-01-01 00:00:01 UTC
+  Created By  created by
+      Source  source
+   Info Hash  b7595205a46491b3e8686e10b28efe7144d066cc
+Torrent Size  240 bytes
+Content Size  20 bytes
+     Private  yes
+    Trackers  b
+              c
+              a
+  Piece Size  16 KiB
+ Piece Count  1
+  File Count  1
+       Files  foo
+";
+
+      assert_eq!(have, want);
+    }
+
+    {
+      let mut env = TestEnvBuilder::new()
+        .arg_slice(&["imdl", "torrent", "show", "--input", "foo.torrent"])
+        .build();
+
+      let path = env.resolve("foo.torrent");
+
+      metainfo.dump(path).unwrap();
+
+      env.run().unwrap();
+
+      let have = env.out();
+      let want = "\
+Name\tfoo
+Comment\tcomment
+Created\t1970-01-01 00:00:01 UTC
+Created By\tcreated by
+Source\tsource
+Info Hash\tb7595205a46491b3e8686e10b28efe7144d066cc
+Torrent Size\t240
+Content Size\t20
+Private\tyes
+Trackers\tb\tc\ta
+Piece Size\t16384
+Piece Count\t1
+File Count\t1
+Files\tfoo
+";
+
+      assert_eq!(have, want);
+    }
+  }
 }
