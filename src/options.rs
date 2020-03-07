cba238470d3c615d49284f4015875ff60361d83f
Casey Rodarmor <casey@rodarmor.com>
2020-03-06 23:25:15 -0800

    Remove `long_help` in favor of `help`
    Maintaining two help strings in order to have a shorter help message
    doesn't seem worth it.
    
    type: documentation

--- src/options.rs
+++ src/options.rs
@@ -5,11 +5,10 @@ pub(crate) struct Options {
   #[structopt(
     long = "unstable",
     short = "u",
-    help = "Enable unstable features.",
-    long_help = "Enable unstable features. To avoid premature stabilization and excessive version \
-                 churn, unstable features are unavailable unless this flag is set. Unstable \
-                 features are not bound by semantic versioning stability guarantees, and may be \
-                 changed or removed at any time."
+    help = "Enable unstable features. To avoid premature stabilization and excessive version \
+            churn, unstable features are unavailable unless this flag is set. Unstable features \
+            are not bound by semantic versioning stability guarantees, and may be changed or \
+            removed at any time."
   )]
   unstable: bool,
   #[structopt(
@@ -17,11 +16,10 @@ pub(crate) struct Options {
     default_value = use_color::AUTO,
     set = ArgSettings::CaseInsensitive,
     possible_values = use_color::VALUES,
-    help = "Print colorful output.",
-    long_help = "Print colorful output. When `auto`, the default, colored output is only enabled \
-                 if imdl detects that it is connected to a terminal, the `NO_COLOR` environment \
-                 variable is not set, and the `TERM` environment variable is not set with a \
-                 value of `dumb`.",
+    help = "Print colorful output. When `auto`, the default, colored output is only enabled \
+            if imdl detects that it is connected to a terminal, the `NO_COLOR` environment \
+            variable is not set, and the `TERM` environment variable is not set with a \
+            value of `dumb`.",
   )]
   pub(crate) use_color: UseColor,
 }
