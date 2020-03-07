e732e0675710c7dc674b3386e4fad1a96d5b2458
Casey Rodarmor <casey@rodarmor.com>
2020-03-06 22:50:04 -0800

    Refactor Opt into Arguments, Options, and Subcommand (#200)

--- src/common.rs
+++ src/common.rs
@@ -51,12 +51,13 @@ pub(crate) use crate::{
 
 // structs and enums
 pub(crate) use crate::{
-  bytes::Bytes, env::Env, error::Error, file_info::FileInfo, file_path::FilePath,
-  file_status::FileStatus, files::Files, hasher::Hasher, info::Info, lint::Lint, linter::Linter,
-  md5_digest::Md5Digest, metainfo::Metainfo, mode::Mode, node::Node, opt::Opt,
-  piece_length_picker::PieceLengthPicker, piece_list::PieceList, platform::Platform,
-  sha1_digest::Sha1Digest, status::Status, style::Style, table::Table, target::Target,
-  torrent_summary::TorrentSummary, use_color::UseColor, verifier::Verifier, walker::Walker,
+  arguments::Arguments, bytes::Bytes, env::Env, error::Error, file_info::FileInfo,
+  file_path::FilePath, file_status::FileStatus, files::Files, hasher::Hasher, info::Info,
+  lint::Lint, linter::Linter, md5_digest::Md5Digest, metainfo::Metainfo, mode::Mode, node::Node,
+  options::Options, piece_length_picker::PieceLengthPicker, piece_list::PieceList,
+  platform::Platform, sha1_digest::Sha1Digest, status::Status, style::Style,
+  subcommand::Subcommand, table::Table, target::Target, torrent_summary::TorrentSummary,
+  use_color::UseColor, verifier::Verifier, walker::Walker,
 };
 
 // type aliases
