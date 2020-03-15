154e26501dc31328163911037325241dab7d0b6f
Casey Rodarmor <casey@rodarmor.com>
2020-03-15 03:22:33 -0700

    Print individual file torrent verification errors
    If torrent verification fails, print all errors with individual files.

--- src/env.rs
+++ src/env.rs
@@ -3,12 +3,8 @@ use crate::common::*;
 pub(crate) struct Env {
   args: Vec<OsString>,
   dir: PathBuf,
-  pub(crate) err: Box<dyn Write>,
-  pub(crate) out: Box<dyn Write>,
-  err_style: Style,
-  out_style: Style,
-  out_is_term: bool,
-  err_is_term: bool,
+  err: OutputStream,
+  out: OutputStream,
 }
 
 impl Env {
@@ -18,34 +14,13 @@ impl Env {
       Err(error) => panic!("Failed to get current directory: {}", error),
     };
 
-    let no_color = env::var_os("NO_COLOR").is_some()
-      || env::var_os("TERM").as_deref() == Some(OsStr::new("dumb"));
+    let style = env::var_os("NO_COLOR").is_none()
+      && env::var_os("TERM").as_deref() != Some(OsStr::new("dumb"));
 
-    let err_style = if no_color || !atty::is(atty::Stream::Stderr) {
-      Style::inactive()
-    } else {
-      Style::active()
-    };
+    let out_stream = OutputStream::stdout(style);
+    let err_stream = OutputStream::stderr(style);
 
-    let out_style = if no_color || !atty::is(atty::Stream::Stdout) {
-      Style::inactive()
-    } else {
-      Style::active()
-    };
-
-    let out_is_term = atty::is(atty::Stream::Stdout);
-    let err_is_term = atty::is(atty::Stream::Stderr);
-
-    Self::new(
-      dir,
-      io::stdout(),
-      out_style,
-      out_is_term,
-      io::stderr(),
-      err_style,
-      err_is_term,
-      env::args(),
-    )
+    Self::new(dir, env::args(), out_stream, err_stream)
   }
 
   pub(crate) fn run(&mut self) -> Result<(), Error> {
@@ -57,40 +32,23 @@ impl Env {
 
     let args = Arguments::from_iter_safe(&self.args)?;
 
-    match args.options().use_color {
-      UseColor::Always => self.err_style = Style::active(),
-      UseColor::Auto => {}
-      UseColor::Never => self.err_style = Style::inactive(),
-    }
+    let use_color = args.options().use_color;
+    self.err.set_use_color(use_color);
+    self.out.set_use_color(use_color);
 
     args.run(self)
   }
 
-  pub(crate) fn new<O, E, S, I>(
-    dir: PathBuf,
-    out: O,
-    out_style: Style,
-    out_is_term: bool,
-    err: E,
-    err_style: Style,
-    err_is_term: bool,
-    args: I,
-  ) -> Self
+  pub(crate) fn new<S, I>(dir: PathBuf, args: I, out: OutputStream, err: OutputStream) -> Self
   where
-    O: Write + 'static,
-    E: Write + 'static,
     S: Into<OsString>,
     I: IntoIterator<Item = S>,
   {
     Self {
       args: args.into_iter().map(Into::into).collect(),
-      err: Box::new(err),
-      out: Box::new(out),
       dir,
-      out_style,
-      out_is_term,
-      err_style,
-      err_is_term,
+      out,
+      err,
     }
   }
 
@@ -109,21 +67,24 @@ impl Env {
           _ => Err(EXIT_FAILURE),
         }
       } else {
+        let style = self.err.style();
         writeln!(
           &mut self.err,
           "{}{}: {}{}",
-          self.err_style.error().paint("error"),
-          self.err_style.message().prefix(),
+          style.error().paint("error"),
+          style.message().prefix(),
           error,
-          self.err_style.message().suffix(),
+          style.message().suffix(),
         )
         .ok();
+
+        error.print_body(self).ok();
 
         if let Some(lint) = error.lint() {
           writeln!(
             &mut self.err,
             "{}: This check can be disabled with `--allow {}`.",
-            self.err_style.message().paint("note"),
+            style.message().paint("note"),
             lint.name()
           )
           .ok();
@@ -144,20 +105,20 @@ impl Env {
     self.dir().join(path).clean()
   }
 
-  pub(crate) fn out_is_term(&self) -> bool {
-    self.out_is_term
+  pub(crate) fn err(&self) -> &OutputStream {
+    &self.err
   }
 
-  pub(crate) fn out_style(&self) -> Style {
-    self.out_style
+  pub(crate) fn err_mut(&mut self) -> &mut OutputStream {
+    &mut self.err
   }
 
-  pub(crate) fn err_is_term(&self) -> bool {
-    self.err_is_term
+  pub(crate) fn out(&self) -> &OutputStream {
+    &self.out
   }
 
-  pub(crate) fn err_style(&self) -> Style {
-    self.err_style
+  pub(crate) fn out_mut(&mut self) -> &mut OutputStream {
+    &mut self.out
   }
 }
 
