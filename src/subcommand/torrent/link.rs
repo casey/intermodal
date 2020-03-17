41229d4728637d70db8f622e1ed122d9e948f4b3
Casey Rodarmor <casey@rodarmor.com>
2020-03-17 05:48:29 -0700

    Remove unused argument

--- src/subcommand/torrent/link.rs
+++ src/subcommand/torrent/link.rs
@@ -279,7 +279,7 @@ mod tests {
     let expected = if cfg!(target_os = "windows") {
       let script = env.resolve("open.bat");
       fs::write(&script, format!("echo \"\" > {}", opened.display())).unwrap();
-      format!("\r\n", link)
+      format!("\r\n")
     } else {
       let script = env.resolve(&Platform::opener().unwrap()[0]);
       fs::write(
