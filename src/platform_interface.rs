114a61978579a95c7484a9b57de1888b8c7df2da
Casey Rodarmor <casey@rodarmor.com>
2020-03-20 14:55:42 -0700

    Use `open` crate to open files and URLs (#270)
    Opening URLs on Windows is very complex, so delegate to the
    `open` crate.

--- src/platform_interface.rs
+++ src/platform_interface.rs
@@ -2,49 +2,22 @@ use crate::common::*;
 
 pub(crate) trait PlatformInterface {
   fn open_file(path: &Path) -> Result<(), Error> {
-    Self::open_raw(path.as_os_str())
+    Self::open_target(path.as_ref())
   }
 
   fn open_url(url: &Url) -> Result<(), Error> {
-    if cfg!(windows) {
-      let escaped = format!("\"{}\"", url);
-      Self::open_raw(escaped.as_str().as_ref())
-    } else {
-      Self::open_raw(url.as_str().as_ref())
-    }
+    Self::open_target(url.as_str().as_ref())
   }
 
-  fn open_raw(target: &OsStr) -> Result<(), Error> {
-    let mut command = Self::opener()?;
-    command.push(OsString::from(target));
+  fn open_target(target: &OsStr) -> Result<(), Error> {
+    let exit_status = open::that(target).context(error::OpenerInvoke)?;
 
-    let command_string = || {
-      command
-        .iter()
-        .map(|arg| arg.to_string_lossy().into_owned())
-        .collect::<Vec<String>>()
-        .join(",")
-    };
-
-    let status = Command::new(&command[0])
-      .args(&command[1..])
-      .status()
-      .map_err(|source| Error::CommandInvoke {
-        source,
-        command: command_string(),
-      })?;
-
-    if status.success() {
-      Ok(())
-    } else {
-      Err(Error::CommandStatus {
-        command: command_string(),
-        status,
-      })
+    if !exit_status.success() {
+      return Err(Error::OpenerExitStatus { exit_status });
     }
-  }
 
-  fn opener() -> Result<Vec<OsString>, Error>;
+    Ok(())
+  }
 
   fn hidden(path: &Path) -> Result<bool, Error>;
 }
